1a01a12b861fb503491e20a86c11fba7f2198a69
marcglm <marcghalem@hotmail.fr>
2021-05-06 00:20:12 +0200

    ajout du use case : createclient

--- src/main/java/model/users/AccountRepository.java
+++ src/main/java/model/users/AccountRepository.java
@@ -5,6 +5,11 @@ import java.util.Optional;
 public interface AccountRepository {
     Optional<Account> findById(Long cliendId);
 
+    Optional<Account> findByEmail(String email);
+
+    Optional<Account> insert(Account client);
+
+
     void updateClientInformation(Client client);
 
     void updateDeliverInformation(Deliver client);
---
+++ src/main/java/use_cases/client/CreateClient.java
@@ -0,0 +1,22 @@
+package use_cases.client;
+
+import model.users.Account;
+import model.users.AccountException;
+import model.users.AccountRepository;
+import model.users.Client;
+
+import java.util.Optional;
+
+public class CreateClient {
+    private final AccountRepository accountRepository;
+
+    public CreateClient(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public void execute(Client client) throws AccountException {
+        Optional<Account> clientFounded = accountRepository.findByEmail(client.getEmail());
+        if(clientFounded.isPresent()) throw new AccountException("User already exist !");
+        accountRepository.insert(client);
+    }
+}
