575ece95b600f3332152e93ee0cf112dd2e2c3a9
Joseph CHATELIN <josephchatelin@gmail.com>
2021-05-06 11:39:06 +0200

    fixed conflicts

--- src/main/java/use_cases/show/ShowRestaurantsAvailable.java
+++ src/main/java/use_cases/show/ShowRestaurantsAvailable.java
@@ -1,4 +1,28 @@
 package use_cases.show;
 
+import model.dishes.Dishes;
+import model.restaurant.Restaurant;
+import model.restaurant.RestaurantRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShowRestaurantsAvailable {
+    private final RestaurantRepository restaurantRepository;
+
+    public ShowRestaurantsAvailable(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    List<Dishes> showRestaurantMenu(Restaurant restaurant){
+        List<Dishes> restaurantMenu = new ArrayList<Dishes>();
+
+        try{
+            restaurantMenu = restaurantRepository.showRestaurantMenu(restaurant);
+        }catch (Error error){
+            System.err.println(error.getMessage());
+        }
+
+        return  restaurantMenu;
+    }
 }
