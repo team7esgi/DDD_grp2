58698ee77eb13aff92d20ab8ce402fbadd8e4162
marcglm <marcghalem@hotmail.fr>
2021-05-06 11:38:57 +0200

    ajout d'(attribut dans command et dishes

--- src/main/java/model/command/Command.java
+++ src/main/java/model/command/Command.java
@@ -1,9 +1,18 @@
 package model.command;
 
+import model.dishes.Dishes;
+
+import java.util.List;
+
 public class Command {
 
-
     private Long id;
+
+    private List<Dishes> dishesList;
+
+    private Long clientId;
+
+    private Long delivererId;
 
     public Long getId() {
         return this.id;
--- src/main/java/model/dishes/Dishes.java
+++ src/main/java/model/dishes/Dishes.java
@@ -1,4 +1,10 @@
 package model.dishes;
 
+import java.util.List;
+
 public class Dishes {
+
+    private Long id;
+
+    private List<String>
 }
